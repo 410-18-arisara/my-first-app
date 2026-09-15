24674ae94a73c86034e5dfc2f3646368c933096a
410-18-arisara <54353@yupparaj.ac.th>
2026-09-15 22:55:20 +0700

    Update bmiiApp.py

--- bmiiApp.py
+++ bmiiApp.py
@@ -14,7 +14,7 @@ if st.button("คำนวณค่า BMI 🎯"):
       st.write("---")
       st.header(f"ค่า BMI ของคุณคือ: **{bmi: .2f}**")
 
-if bmi < 18.5
+if bmi < 18.5:
       st.warning(" คุณมีน้ำหนักน้อยกว่าเกณฑ์ (ผอม)")
 elif bmi < 23.0:
       st.success = "สุขภาพดี"
